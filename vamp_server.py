d55300a171ff8688bc87fa14317c49ee06dbfa65
Hemanth Kumar S <hemanthkumar353535@gmail.com>
2021-05-14 22:46:41 +0530

    Update vamp_server.py

--- vamp_server.py
+++ vamp_server.py
@@ -44,10 +44,10 @@ def crct_configuration():
 def db_connect():
     try:
         mydb = mysql.connector.connect(
-            host="localhost",
-            database="voice_assist_db",
-            user="voice_assist",
-            password="Waycool@12345")
+            host="",
+            database="",
+            user="",
+            password="")
         
         print('[info] : Database Connection Successful!!! :)')
         return mydb,1;
